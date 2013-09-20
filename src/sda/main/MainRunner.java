79143d0708b7961f07b6120b5c5d20a40cbbcdd8
Alin Taranu - Desktop <alin.taranu@gmail.com>
2013-09-21 00:32:01 +0200

    add bubble

---
+++ src/sda/main/MainRunner.java
@@ -0,0 +1,14 @@
+package sda.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alin-Desktop
+ * Date: 9/20/13
+ * Time: 9:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainRunner {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
